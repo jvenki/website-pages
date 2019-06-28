1540d9312a588222326af63ce9c93436f327f136
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2019-06-28 11:49:14 +0530

    Handling Invalid HTML

--- src/migrator/Cleanser.js
+++ src/migrator/Cleanser.js
@@ -3,17 +3,14 @@ const minify = require("html-minifier").minify;
 const winston = require("winston");
 const MigrationError = require("./MigrationError");
 
-const minifyHtml = (html) => {
-    try {
-        return minify(html, {collapseWhitespace: true, removeComments: true, removeEmptyAttributes: true, removeRedundantAttributes: true});
-    } catch (err) {
-        throw new MigrationError(MigrationError.Code.HTML_PARSE_ERROR);
-    }
-};
-
 class Cleanser {
     cleanse(html) {
         winston.verbose("\tCleaning our HTML...");
+        winston.debug("Given HTML\n" + html);
+        html = makeHTMLValid(html);
+        const cleansedHtml = minifyHtml(html);
+        winston.debug("Cleansed HTML\n" + cleansedHtml);
+
         const cleansers = [
             removeEmptyNodesAndEmptyLines, 
             removeUnncessaryRootElement, 
@@ -22,12 +19,33 @@ class Cleanser {
             removeDisqusElements,
             removeOfferTableElements
         ];
-        const cleansedHtml = minifyHtml(html);
         const $ = cheerio.load(cleansedHtml, {decodeEntities: false});
         cleansers.forEach((cleanser) => cleanser($));
+
         return $.html();
     }
 }
+
+const makeHTMLValid = (html) => {
+    // The below cleanup is required in 33 and 187
+    let cleansedHtml = 
+        html.replace(/“/g, "\"").replace(/’/g, "'").replace(/‘/g, "'")
+            .replace(/<<\s*>>/g, /&lt;&lt; &gt;&gt;/)
+            .replace(/<table table-curved">/, "<table class=\"table table-curved\">")
+            .replace(/< Rs/g, "&lt; Rs");
+    cleansedHtml = minify(cleansedHtml, {collapseWhitespace: true, removeComments: true, continueOnParseError: true});
+    cleansedHtml = cleansedHtml.replace(/<<div/g, "<div");
+    return cleansedHtml;
+};
+
+const minifyHtml = (html) => {
+    try {
+        return minify(html, {collapseWhitespace: true, removeComments: true, removeEmptyAttributes: true, removeRedundantAttributes: true});
+    } catch (err) {
+        winston.debug("Unable to Parse the HTML\n" + err);
+        throw new MigrationError(MigrationError.Code.HTML_PARSE_ERROR);
+    }
+};
 
 const removeEmptyNodesAndEmptyLines = ($) => {
     $("*").each((i, e) => {
@@ -49,7 +67,8 @@ const removeUnncessaryRootElement = ($) => {
     let recheck = false;
     $("*").each((i, e) => {
         const $e = $(e);
-        const hasUnncessaryRootDiv = ["primary-txt", "article-txt", "product-content", "bank-prod-page"].some((cn) => $e.hasClass(cn));
+        // h tag was used in #39
+        const hasUnncessaryRootDiv = ["primary-txt", "article-txt", "product-content", "bank-prod-page", "h"].some((cn) => $e.hasClass(cn) || $e.get(0).tagName == cn);
         if (hasUnncessaryRootDiv) {
             $($e.html()).insertAfter($e);
             $e.remove();
@@ -65,7 +84,7 @@ const removeUnncessaryRootElement = ($) => {
 const removeTableOfContents = ($) => {
     $("table").each((i, t) => {
         $(t).find("a").each((j, a) => {
-            if ($(a).attr("href").startsWith("#")) {
+            if ($(a).attr("href") && $(a).attr("href").startsWith("#")) {
                 const $parent = $(a).parent();
                 $(a).remove();
                 removeEmptyAncestors($parent);
