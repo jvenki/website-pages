a969f9d361ac3355f9cc27a23c256dc05572ee8d
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-16 15:16:51 +0530

    Migrator  Framework - Migrating content from HTML to JSON

---
+++ src/migrator/DomWalker.js
@@ -0,0 +1,68 @@
+// @flow
+import cheerio from "cheerio";
+import {logger} from "./Logger";
+import MigrationError, {ErrorCode, ConversionIssueCode} from "./MigrationError";
+import {findHandlerForElement} from "./handlers";
+import {computeNodeName, computePathNameToElem} from "./handlers/Utils";
+
+type CheerioElemType = Function;
+type CheerioDocType = Function;
+
+export default class DomWalker {
+    static for(html: string, onElement: (elem: Object) => void, onIssue: (err: Error) => void, onOpLog: (converterName: string, source: Array<CheerioElemType>, target: Array<Object>) => void) {
+        return new DomWalker(html, onElement, onIssue, onOpLog);
+    }
+
+    html: string;
+    $: CheerioDocType;
+    $currElem: CheerioElemType;
+    onElement: (elem: Object) => void;
+    onIssue: (err: Error) => void;
+    onOpLog: (converterName: string, source: Array<CheerioElemType>, target: Array<Object>) => void;
+
+    constructor(html: string, onElement: (elem: Object) => void, onIssue: (err: Error) => void, onOpLog: (converterName: string, source: Array<CheerioElemType>, target: Array<Object>) => void) {
+        this.html = html;
+        this.onElement = onElement;
+        this.onIssue = onIssue;
+        this.onOpLog = onOpLog;
+    }
+
+    execute() {
+        logger.verbose("    Walking our DOM...");
+
+        const $ = cheerio.load(this.html, {decodeEntities: false});
+        const rootElements = $("body").contents();
+        if (rootElements.length == 0) {
+            throw new MigrationError(ErrorCode.DOM_EMPTY);
+        }
+
+        let i=0;
+        for (i=0; i<rootElements.length;) {
+            const currElem = $(rootElements[i]);
+            const elemsProcessed = handleElement(currElem, $, this.onElement, this.onIssue, this.onOpLog);
+            i+=elemsProcessed.length;
+        }
+        return this;
+    }
+}
+
+const handleElement = ($element, $, onElement, onIssue, onOpLog) => {
+    const handler = findHandlerForElement($($element));
+    logger.debug(`        Processing Node '${computeNodeName($element)}' : Identified Handler as '${handler.getName()}' : Path from BODY = '${computePathNameToElem($element)}'`);
+
+    const sourceElements = handler.walkToPullRelatedElements($element, $);
+    const {targetElements, issues} = handler.execute(sourceElements, $);
+    if (targetElements == undefined) {
+        onIssue(new MigrationError(ConversionIssueCode.EMPTY_ELEMENT, issues.join(","), $element.toString()));
+    } else {
+        targetElements.forEach((convertedElement) => onElement(convertedElement));
+        if (issues.length > 0) {
+            onIssue(new MigrationError(ConversionIssueCode.OTHERS, issues.join(","), $element.toString()));
+        }
+    }
+
+    onOpLog(handler.getName(), sourceElements, targetElements);
+    logger.debug(`            ${sourceElements.join("")}`);
+    logger.debug(`            ${JSON.stringify(targetElements)}`);
+    return sourceElements;
+};
---
+++ src/migrator/MongoClient.js
@@ -0,0 +1,53 @@
+const MongoNativeClient = require("mongodb").MongoClient;
+import {logger} from "./Logger";
+import {cloneDeep} from "lodash";
+
+export default class MongoDBClient {
+    connect() {
+        return MongoNativeClient.connect("mongodb://localhost:27017")
+            .then((client) => {
+                logger.verbose("Connected successfully to MongoDB");
+                const db = client.db("pages");
+                this.nativeClient = client;
+                this.collection = db.collection("pages");
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    save(doc) {
+        const serializeError = (err) => ({code: err.code, message: err.message, payload: err.payload});
+        const serializedDoc = cloneDeep(JSON.parse(JSON.stringify(doc)));
+        if (serializedDoc.conversionError) {
+            serializedDoc.conversionError = serializeError(serializedDoc.conversionError);
+        }
+        if (serializedDoc.conversionIssues) {
+            serializedDoc.conversionIssues = serializedDoc.conversionIssues.map((i) => serializeError(i));
+        }
+        return this.collection.replaceOne({id: serializedDoc.id}, serializedDoc, { upsert: true });
+    }
+
+    get(id) {
+        return this.collection.findOne({id: parseInt(id)});
+    }
+
+    getAll(startingOffset) {
+        return new Promise((resolve, reject) => {
+            const rows = this.collection.find({}).sort({id: 1}).skip(startingOffset || 0).limit(50).toArray();
+            resolve(rows);
+        });
+    }
+
+    saveValidationResult(id, status, comments) {
+        return this.collection.findOneAndUpdate({id}, {$set: {validationStatus: status, validationComments: comments}});
+    }
+
+    purge() {
+        return this.collection.deleteMany({});
+    }
+
+    disconnect() {
+        this.nativeClient.close();
+    }
+}
