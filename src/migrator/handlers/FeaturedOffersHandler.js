e4555732ae1a2481b3e3ee765c80c00868fc8048
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-17 09:24:15 +0530

    Migrator  Framework - Handlers required for personal-loan (856)

--- src/migrator/handlers/FeaturedOffersHandler.js
+++ src/migrator/handlers/FeaturedOffersHandler.js
@@ -52,3 +52,53 @@ export class FeaturedOffersHandlerVariant_BorderBlue extends BaseHandler {
         return {elements: [{type: "featured-offers", offers}]};
     }
 }
+
+
+export class FeaturedOffersHandlerVariant_Template extends BaseHandler {
+    isCapableOfProcessingElement($e: CheerioElemType) {
+        return $e.get(0).tagName == "div" && $e.hasClass("col-md-12") && $e.hasClass("top-pad-10") 
+            && $e.children().eq(0).get(0).tagName == "h2"
+            && $e.find("img").length > 0;
+    }
+
+    walkToPullRelatedElements($element: CheerioElemType, $: CheerioDocType): Array<CheerioElemType> {
+        const elements = [$element];
+        let $currElem = $element;
+        while (true) {
+            const $nextElement = $currElem.next();
+            if ($nextElement.length == 0) {
+                break;
+            }
+            if (!$nextElement.hasClass("col-md-12") || !$nextElement.hasClass("top-pad-10")) {
+                break;    
+            }
+            elements.push($nextElement);
+            $currElem = $nextElement;
+        }
+        return elements;
+    }
+
+    validate($element: CheerioElemType, $: CheerioDocType) {
+        assert($element.find("h2").length == 1, "FeaturedOffersHandlerVariant_Template-ConditionNotMet#1", $element);
+        assert($element.find("img").length == 1, "FeaturedOffersHandlerVariant_Template-ConditionNotMet#2", $element);
+        assert($element.find(" > div").length == 2, "FeaturedOffersHandlerVariant_Template-ConditionNotMet#3", $element);
+    }
+
+    convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
+        const extract = ($offerElement) => {
+            const title = extractHeadingText($offerElement.find("h2"), $);
+            const imgSrc = extractImgSrc($offerElement.find("img"));
+            let body = "";
+            $offerElement.find("> div").eq(1).children().each((i, be) => {
+                if (be.tagName == "h5" && $(be).text() == "Key Highlights") {
+                    return;
+                }
+                body += extractContentHtml($(be), $);
+            });
+            assert(Boolean(title) && Boolean(imgSrc) && Boolean(body), "FeaturedOffersHandlerVariant_Template-ConditionNotMet#4", $offerElement);
+            return {title, img: {src: imgSrc}, body};
+        };
+        const offers = elements.map(($element) => extract($element));
+        return {elements: [{type: "featured-offers", offers}]};
+    }
+}
