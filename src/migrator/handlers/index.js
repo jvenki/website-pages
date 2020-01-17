ae3bacefcc9f2466ecc6bf5073f6c23d140302c6
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-17 09:24:15 +0530

    Migrator  Framework - Handlers required for personal-loan (856)

--- src/migrator/handlers/index.js
+++ src/migrator/handlers/index.js
@@ -9,23 +9,28 @@ import TextHandler from "./TextHandler";
 import {JumbotronHandler} from "./JumbotronHandler";
 import {VideoHandler} from "./VideoHandler";
 import UnwrapHandler from "./UnwrapHandler";
-import {CTAHandlerVariant_ProductLandingBlock} from "./CTAHandler";
+import NoopWarningHandler from "./NoopWarningHandler";
+import {CTAHandlerVariant_ProductLandingBlock, CTAHandlerVariant_LonelyLink} from "./CTAHandler";
 import { AccordionHandler } from "./AccordionHandler";
-import {FeaturedOffersHandlerVariant_BorderBlue} from "./FeaturedOffersHandler";
-import {FAQHandlerVariant_HeadingRegexAndDivWithSchema} from "./FAQHandler";
+import {FeaturedOffersHandlerVariant_BorderBlue, FeaturedOffersHandlerVariant_Template} from "./FeaturedOffersHandler";
+import {FAQHandlerVariant_HeadingRegexAndDivWithSchema, FAQHandlerVariant_HeadingRegexFollowedByPs} from "./FAQHandler";
 import { DeprecatedTOCHandlerVariant_ProductsInvest, DeprecatedTOCHandlerVariant_ULofAsOnly } from "./DeprecatedTOCHandler";
 
 const handlers = [
     new DeprecatedTOCHandlerVariant_ProductsInvest(),
     new DeprecatedTOCHandlerVariant_ULofAsOnly(),
     new FAQHandlerVariant_HeadingRegexAndDivWithSchema(),
+    new FAQHandlerVariant_HeadingRegexFollowedByPs(),
     new SectionHandler(), 
+    new CTAHandlerVariant_ProductLandingBlock(),
+    new CTAHandlerVariant_LonelyLink(),
     new TextHandler(),
     new AccordionHandler(),
     new JumbotronHandler(),
     new VideoHandler(),
     new FeaturedOffersHandlerVariant_BorderBlue(),
-    new CTAHandlerVariant_ProductLandingBlock(),
+    new FeaturedOffersHandlerVariant_Template(),
+    new NoopWarningHandler(),
     new UnwrapHandler()
 ];
 
