2459480734d19db6f548dd8a7e4bb8b5a2d13d49
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-18 08:53:53 +0530

    Migrator  Framework - Handlers required for icici-personal-loan (4)

---
+++ src/migrator/handlers/BlockQuoteHandler.js
@@ -0,0 +1,20 @@
+// @flow
+import type {CheerioDocType, CheerioElemType, ConversionResultType} from "./BaseHandler";
+import BaseHandler from "./BaseHandler";
+import {extractHeadingText, extractContentHtml, isElementAHeadingNode} from "./Utils";
+
+export default class BlockQuoteHandler extends BaseHandler {
+    isCapableOfProcessingElement($element: CheerioElemType): boolean {
+        return $element.get(0).tagName == "blockquote";
+    }
+
+    convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
+        const $element = elements[0];
+        let title;
+        if (isElementAHeadingNode($element.children().eq(0))) {
+            title = extractHeadingText($element.children().eq(0), $);
+        }
+        const body = $element.children().slice(title ? 1 : 0).map((i, e) => extractContentHtml($(e), $)).get().join("");
+        return {elements: [{type: "blockquote", title, body}]};
+    }
+}
---
+++ src/migrator/handlers/ReferencesHandler.js
@@ -0,0 +1,34 @@
+// @flow
+import type {CheerioDocType, CheerioElemType, ConversionResultType} from "./BaseHandler";
+import BaseHandler from "./BaseHandler";
+import { extractHeadingText, extractLinkText } from "./Utils";
+
+export class ReferencesHandlerVariant_Nav extends BaseHandler {
+    isCapableOfProcessingElement($element: CheerioElemType) {
+        return $element.get(0).tagName == "nav";
+    }
+
+    convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
+        const $element = elements[0];
+        const title = extractHeadingText($element.find("h2,h3,h4,h5,h6,h7").eq(0), $);
+        const items = $element.find("li > a").map((i, link) => ({link: $(link).attr("href"), title: extractLinkText($(link), $)})).get();
+        return {elements: [{type: "references", title, items}]};
+    }
+}
+
+export class ReferencesHandlerVariant_HeadingRegex extends BaseHandler {
+    isCapableOfProcessingElement($element: CheerioElemType) {
+        const headingRegex = /related [a-z]* product/i;
+        return $element.text().match(headingRegex) && $element.next().get(0).tagName == "table";
+    }
+
+    walkToPullRelatedElements($element: CheerioElemType, $: CheerioDocType): Array<CheerioElemType> {
+        return [$element, $element.next()];
+    }
+
+    convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
+        const title = extractHeadingText(elements[0], $);
+        const items = elements[1].find("a").map((i, link) => ({link: $(link).attr("href"), title: extractLinkText($(link), $)})).get();
+        return {elements: [{type: "references", title, items}]};
+    }
+}
