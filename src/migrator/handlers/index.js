996320ce2ee12d35e4cae2cd5e286038dbd350ea
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-18 09:17:26 +0530

    Migrator  Framework - Handlers required for hdfc-personal-loan (5)

--- src/migrator/handlers/index.js
+++ src/migrator/handlers/index.js
@@ -14,7 +14,7 @@ import NoopWarningHandler from "./NoopWarningHandler";
 import {CTAHandlerVariant_ProductLandingBlock, CTAHandlerVariant_LonelyLink} from "./CTAHandler";
 import { AccordionHandler } from "./AccordionHandler";
 import {FeaturedOffersHandlerVariant_BorderBlue, FeaturedOffersHandlerVariant_Template} from "./FeaturedOffersHandler";
-import {FAQHandlerVariant_HeadingRegexAndDivWithSchema, FAQHandlerVariant_HeadingRegexFollowedByPs, FAQHandlerVariant_HeadingRegexFollowedByDetails} from "./FAQHandler";
+import {FAQHandlerVariant_HeadingRegexAndDivWithSchema, FAQHandlerVariant_HeadingRegexFollowedByPs, FAQHandlerVariant_HeadingRegexFollowedByDetails, FAQHandlerVariant_HeadingRegexFollowedByDivOfDetails} from "./FAQHandler";
 import { DeprecatedTOCHandlerVariant_ProductsInvest, DeprecatedTOCHandlerVariant_ULofAsOnly } from "./DeprecatedTOCHandler";
 import GridHandler from "./GridHandler";
 import BlockQuoteHandler from "./BlockQuoteHandler";
@@ -28,6 +28,7 @@ const handlers = [
     new FAQHandlerVariant_HeadingRegexAndDivWithSchema(),
     new FAQHandlerVariant_HeadingRegexFollowedByPs(),
     new FAQHandlerVariant_HeadingRegexFollowedByDetails(),
+    new FAQHandlerVariant_HeadingRegexFollowedByDivOfDetails(),
     new SectionHandler(), 
     new CTAHandlerVariant_ProductLandingBlock(),
     new CTAHandlerVariant_LonelyLink(),
