a29201b297b313960e49379447754bb050ea247a
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-01-28 10:45:55 +0530

    Migrator Framework - Handlers required for uco-personal-loan (57)

--- src/migrator/handlers/FAQHandler.js
+++ src/migrator/handlers/FAQHandler.js
@@ -277,7 +277,7 @@ export class FAQInsideAccordionPanelHandler extends FAQBaseHandler {
     convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
         const title = extractHeadingText(elements[0].find(".panel-title a"), $);
         const panelBody = elements[0].find(".panel-body");
-        const items = panelBody.find("ul > h3, ul > li > h3, ol > li > h3").map((i, q) => {
+        const items = panelBody.find("ul > h3, ul > li > h3, ol > li > h3, ol > li > strong").map((i, q) => {
             const $q = $(q);
             const qns = $q.parent().get(0).tagName == "ul" ? extractHeadingText($q.find("li"), $) : extractHeadingText($q, $);
             const ans = $q.nextUntil("h3,li").map((i, a) => extractContentHtml($(a), $)).get().join("");
