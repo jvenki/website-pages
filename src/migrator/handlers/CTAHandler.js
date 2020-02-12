c782421144c53e9811b4e4928b1ec3dd87c8ca85
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-02-12 16:40:10 +0530

    MassMigrationFix - Made CTAHandlerVariant_ProductLandingBlock lenient

--- src/migrator/handlers/CTAHandler.js
+++ src/migrator/handlers/CTAHandler.js
@@ -12,9 +12,10 @@ export class CTAHandlerVariant_ProductLandingBlock extends BaseHandler {
         assert($element.children().length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#1", $element);
         assert($element.find("div.link-section").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#2", $element);
         assert($element.find("div.link-section").children().length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#3", $element);
-        assert($element.find("div.link-section span").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#4", $element);
-        assert($element.find("div.link-section span").children().length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#5", $element);
-        assert($element.find("div.link-section span a").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#6", $element);
+        //assert($element.find("div.link-section span").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#4", $element);
+        //assert($element.find("div.link-section span").children().length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#5", $element);
+        // assert($element.find("div.link-section span a").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#6", $element);
+        assert($element.find("div.link-section a").length == 1, "CTAHandlerVariant_ProductLandingBlock-ConditionNotMet#6", $element);
     }
 
     convert(elements: Array<CheerioElemType>, $: CheerioDocType): ConversionResultType {
