c266e9959988a8f529025ec701dd237542e1c70e
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-02-19 22:46:52 +0530

    MassMigrationFix - Fixed q tag

--- src/migrator/handlers/BlockQuoteHandler.js
+++ src/migrator/handlers/BlockQuoteHandler.js
@@ -14,7 +14,7 @@ export default class BlockQuoteHandler extends BaseHandler {
         if (isElementAHeadingNode($element.children().eq(0))) {
             title = extractHeadingText($element.children().eq(0), $);
         }
-        const body = $element.children().slice(title ? 1 : 0).map((i, e) => extractContentHtml($(e), $)).get().join("");
+        const body = $element.contents().slice(title ? 1 : 0).map((i, e) => extractContentHtml($(e), $)).get().join("");
         assert(Boolean(body), "BlockQuoteHandler-ConditionNotMet#1", $element);
         return {elements: [{type: "blockquote", title, body}]};
     }
