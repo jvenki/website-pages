cc4c2310158f9d195e0fc79a07858688048106ac
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2020-03-10 11:05:14 +0530

    View

--- src/view/page/elements/ResponsiveTable.js
+++ src/view/page/elements/ResponsiveTable.js
@@ -20,16 +20,18 @@ export default class ResponsiveTable extends React.Component {
 
         const bodyRows = $("table > tbody > tr").map((i, tr) => {
             return (
-                <div key={i} className="tabular-data tb-priority">
-                    {$(tr).find(">td").map((j, td) => <div key={`${i}-${j}`} className="tabular-column">{computeCellHtml($(td), $)}</div>).get()}
+                <div key={i} className={`tabular-data ${i%2 == 1 ? "tb-priority" : ""}`}>
+                    {$(tr).find(">td").map((j, td) => <div key={`${i}-${j}`} className="tabular-column grid-col-1">{computeCellHtml($(td), $)}</div>).get()}
                 </div>
             );
         }).get();
 
         return (
-            <div className="tabular-section tb-pad-10">
-                {headerRows}
-                {bodyRows}
+            <div className="container">
+                <div className="tabular-section tb-pad-10">
+                    {headerRows}
+                    {bodyRows}
+                </div>
             </div>
         );
     }
